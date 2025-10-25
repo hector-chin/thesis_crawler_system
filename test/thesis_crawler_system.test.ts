c1ec5ecc1554a529099aee83de2466ca92e64ac4
hector-chin <changtung.chin@gmail.com>
2025-10-25 16:31:45 +0800

    Initial commit

---
+++ test/thesis_crawler_system.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ThesisCrawlerSystem from '../lib/thesis_crawler_system-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/thesis_crawler_system-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ThesisCrawlerSystem.ThesisCrawlerSystemStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
