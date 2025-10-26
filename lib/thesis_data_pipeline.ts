fe370c038a2268dc8eded7d570283d6addf63838
hector-chin <changtung.chin@gmail.com>
2025-10-26 18:22:22 +0800

    feat: Modified Lambda info and LogGroup implementation

--- lib/thesis_data_pipeline.ts
+++ lib/thesis_data_pipeline.ts
@@ -8,29 +8,35 @@ import * as logs from 'aws-cdk-lib/aws-logs';
 export class ThesisDataPipeline extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+        const functionName = 'ThesisDataCrawlerFunction';
         const lambdaFunction = new lambda.Function(this, 'ThesisLambdaFunction', {
-            functionName: 'ThesisDataCrawlerFunction',
+            functionName: functionName,
             runtime: lambda.Runtime.PYTHON_3_12,
+            description: 'Lambda function for crawling arXiv.org thesis',
             handler: 'thesis_crawler.handler',
+            memorySize: 1024,
+            ephemeralStorageSize: cdk.Size.mebibytes(32),
+            timeout: cdk.Duration.minutes(10),
             code: lambda.Code.fromAsset('../thesis_crawler_system/lambda_code/'),
+            logGroup: new logs.LogGroup(this, 'ThesisLambdaLogGroup', {
+                logGroupName: `/aws/lambda/${functionName}`,
+                retention: logs.RetentionDays.ONE_MONTH,
+                removalPolicy: cdk.RemovalPolicy.DESTROY,
+            })
         });
-        new logs.LogGroup(this, 'ThesisLambdaLogGroup', {
-            logGroupName: `/aws/lambda/${lambdaFunction.functionName}`,
-            retention: logs.RetentionDays.ONE_MONTH,
-            removalPolicy: cdk.RemovalPolicy.DESTROY,
-        })
 
         // EventBridge Rule to trigger Lambda on schedule
         const rule = new Rule(this, 'ThesisDataPipelineTriggerRule', {
             ruleName: 'ThesisDataPipelineTriggerRule',
             schedule: Schedule.cron({ minute: '0', hour: '0', day: '*', month: '*', year: '*' }),
+            // logGroup: '',
         })
         // EventBridge Rule Log Group
-        new logs.LogGroup(this, 'EventBridgeRuleLogGroup', {
-            logGroupName: `/aws/events/${rule.ruleName}`,
-            retention: logs.RetentionDays.ONE_MONTH,
-            removalPolicy: cdk.RemovalPolicy.DESTROY,
-        });
+        // new logs.LogGroup(this, 'EventBridgeRuleLogGroup', {
+        //     logGroupName: `/aws/events/${rule.ruleName}`,
+        //     retention: logs.RetentionDays.ONE_MONTH,
+        //     removalPolicy: cdk.RemovalPolicy.DESTROY,
+        // });
         rule.addTarget(new targets.LambdaFunction(lambdaFunction));
     }
 }
