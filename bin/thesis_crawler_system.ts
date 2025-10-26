75089ab1f83040ef8048982c7a9d8c54ab5a0e26
hector-chin <changtung.chin@gmail.com>
2025-10-26 19:42:19 +0800

    hotfix: extract env variables

--- bin/thesis_crawler_system.ts
+++ bin/thesis_crawler_system.ts
@@ -1,27 +1,16 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { ThesisCrawlerSystemStack } from '../lib/thesis_crawler_system-stack';
+import { ThesisSchedularMonitor } from '../lib/thesis_schedular_monitor';
 import { ThesisDataPipeline } from '../lib/thesis_data_pipeline';
 
 const app = new cdk.App();
+console.log(process.env.CDK_DEFAULT_ACCOUNT);
+console.log(process.env.CDK_DEFAULT_REGION);
 
 new ThesisDataPipeline(app, 'ThesisDataPipelineStack', {
-  env: { account: '411767351069', region: 'ap-northeast-1' },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
 
-new ThesisCrawlerSystemStack(app, 'ThesisCrawlerSystemStack', {
-  env: { account: '411767351069', region: 'ap-northeast-1' },
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new ThesisSchedularMonitor(app, 'ThesisSchedularMonitorStack', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
